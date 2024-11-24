4cbbfa7aa03ad7343e1ac59deebf39c57b01cc32
IraMira333 <irinat333@gmail.com>
2024-11-24 23:08:07 +0200

    Updated pages with sanityFetch

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/sanity/lib/client";
+import { client, sanityFetch } from "@/sanity/lib/client";
 import { POST_QUERY, POSTS_SLUGS_QUERY } from "@/sanity/lib/queries";
 import { Post } from "@/components/Post";
 import { notFound } from "next/navigation";
@@ -10,16 +10,16 @@ export async function generateStaticParams() {
 
   return slugs;
 }
-const options = { next: { revalidate: 60 } };
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
-
-  const post = await client.fetch(POST_QUERY, { slug }, options);
+  const post = await sanityFetch({
+    query: POST_QUERY,
+    params,
+  });
 
   if (!post) {
     notFound();
